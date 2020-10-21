5b8152f7efa0ee8aa193ce1dc601df5c3da4310d
Breizhcat <yleroux@gmail.com>
2020-10-21 14:51:33 +0200

    [ADD] Header for last function

--- Sandbox.lua
+++ Sandbox.lua
@@ -22,7 +22,8 @@ return {
 	},
 	
 	execute = function(dz, device)
-
-        dz.helpers.sendTelegramPicture(dz, "ploufette", "https://test.psychologies.com/var/tests/storage/images/2/8/4/0/480482-2-fre-FR/Ce_que_revele_votre_facon_de_parler_thumb_434_265.jpg")
+        if dz.devices(75).state == dz.SECURITY_ARMEDAWAY then
+            dz.helpers.sendTelegramPicture(dz, "ploufette", "https://test.psychologies.com/var/tests/storage/images/2/8/4/0/480482-2-fre-FR/Ce_que_revele_votre_facon_de_parler_thumb_434_265.jpg")    
+        end
 	end
 }
